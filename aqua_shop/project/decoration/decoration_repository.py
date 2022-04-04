ec3553811de5898123be0ab097f15342e3717645
Atanas Atanasov <atanas4537@gmail.com>
2022-04-04 18:48:58 +0300

    Implemented decoration classes

---
+++ aqua_shop/project/decoration/decoration_repository.py
@@ -0,0 +1,21 @@
+from project.decoration.base_decoration import BaseDecoration
+
+
+class DecorationRepository:
+    def __init__(self):
+        self.decorations = []
+
+    def add(self, decoration: BaseDecoration):
+        self.decorations.append(decoration)
+
+    def remove(self, decoration: BaseDecoration):
+        if decoration not in self.decorations:
+            return False
+        self.decorations.remove(decoration)
+        return True
+
+    def find_by_type(self, decoration_type: str):
+        for decoration in self.decorations:
+            if decoration.__class__.__name__ == decoration_type:
+                return decoration
+        return "None"
